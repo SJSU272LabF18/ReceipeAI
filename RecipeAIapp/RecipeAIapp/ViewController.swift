a8c155be0a62aef87f3b81b21f8a3a0e30319363
savipate <patel.sajan.v@gmail.com>
2018-12-04 16:54:15 -0800

    Project Creation

---
+++ RecipeAIapp/RecipeAIapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RecipeAIapp
+//
+//  Created by Sajan on 12/4/18.
+//  Copyright © 2018 Sajan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
