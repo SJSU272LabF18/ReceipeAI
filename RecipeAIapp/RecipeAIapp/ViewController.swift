42abd1b4ea69deea1646560d5ff424e6ea338147
Sajan Patel <patel.sajan.v@gmail.com>
2018-12-05 18:32:08 -0800

    Storyboard fixed

--- RecipeAIapp/RecipeAIapp/ViewController.swift
+++ RecipeAIapp/RecipeAIapp/ViewController.swift
@@ -6,15 +6,53 @@
 //  Copyright © 2018 Sajan. All rights reserved.
 //
 
+
 import UIKit
+import AVKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, FrameExtractorDelegate{
+    
+    var frameExtractor: FrameExtractor!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        /*let captureSession = AVCaptureSession()
+         captureSession.sessionPreset = .photo
+         
+         guard let captureDevice = AVCaptureDevice.default(for: .video) else{return}
+         
+         guard let input = try? AVCaptureDeviceInput(device: captureDevice) else{return}
+         
+         captureSession.addInput(input)
+         
+         captureSession.startRunning()
+         
+         let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+         
+         view.layer.addSublayer(previewLayer)
+         previewLayer.frame = view.frame
+         
+         */
+        frameExtractor = FrameExtractor()
+        frameExtractor.delegate = self
     }
-
-
+    
+    
+    
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    func captured(image: UIImage) {
+        imageView.image = image
+    }
+    
+    
+    
+    
 }
+
 
